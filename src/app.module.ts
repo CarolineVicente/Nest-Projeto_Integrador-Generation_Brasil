d5dbdfb683afab0b67bdf31f509cf8806cf63d44
Caroline Vicente <vicente.caroline@hotmail.com>
2022-09-14 16:59:47 -0300

    adicionando outras tabelas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Carrinho } from './carrinho/entities/carrinho.entity';
+import { CarrinhoModule } from './carrinho/modules/carrinho.module';
+import { Categoria } from './categorias/entities/categoria.entity';
+import { CategoriaModule } from './categorias/modules/categoria.module';
 import { Produto } from './produtos/entities/produto.entity';
 import { ProdutoModule } from './produtos/module/produto.module';
+import { Usuario } from './usuario/entities/usuario.entity';
+import { UsuarioModule } from './usuario/modules/usuario.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -11,12 +17,15 @@ import { ProdutoModule } from './produtos/module/produto.module';
   username: 'root', 
   password: 'root',
   database: 'db_uana',
-  entities: [Produto],
+  entities: [Produto, Categoria, Usuario, Carrinho],
   synchronize: true
   }),
-  ProdutoModule
+  ProdutoModule,
+  CategoriaModule,
+  UsuarioModule,
+  CarrinhoModule
 ],  
-    controllers: [],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
