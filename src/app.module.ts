e66df6800c79cfe8a0668378a7110a1749fe1907
CarolineVicente <vicente.caroline@hotmail.com>
2022-09-15 22:23:23 -0300

    CRUD completo categoria e produto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,27 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Carrinho } from './carrinho/entities/carrinho.entity';
-import { CarrinhoModule } from './carrinho/modules/carrinho.module';
-import { Categoria } from './categorias/entities/categoria.entity';
-import { CategoriaModule } from './categorias/modules/categoria.module';
-import { Produto } from './produtos/entities/produto.entity';
-import { ProdutoModule } from './produtos/module/produto.module';
-import { Usuario } from './usuario/entities/usuario.entity';
-import { UsuarioModule } from './usuario/modules/usuario.module';
+import { AppController } from './app.controller';
+import { Categoria } from './categoria/entities/categoria.entity';
+import { CategoriaModule } from './categoria/modules/categoria.module';
+import { Tarefa } from './tarefa/entities/tarefa.entity';
+import { TarefaModule } from './tarefa/modules/tarefa.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306, 
-  username: 'root', 
-  password: 'root',
-  database: 'db_uana',
-  entities: [Produto, Categoria, Usuario, Carrinho],
-  synchronize: true
-  }),
-  ProdutoModule,
-  CategoriaModule,
-  UsuarioModule,
-  CarrinhoModule
-],  
-  controllers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_todo',
+      entities: [Tarefa, Categoria],
+      synchronize: true
+    }),
+    TarefaModule,
+    CategoriaModule
+  ],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
--- src/categoria/controllers/categoria.controller.ts
+++ src/categoria/controllers/categoria.controller.ts
@@ -1,46 +1,47 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
 import { Categoria } from "../entities/categoria.entity";
 import { CategoriaService } from "../services/categoria.service";
 
+@ApiTags('Categoria')
 @Controller('/categoria')
-export class CategoriaController{
-    constructor(private readonly service: CategoriaService){}
+export class CategoriaController {
+    constructor(private readonly service: CategoriaService) {}
 
     @Get()
     @HttpCode(HttpStatus.OK)
-    findAll(): Promise<Categoria[]>{
+    findAll(): Promise<Categoria[]> {
         return this.service.findAll()
     }
 
-    @Get()
+    @Get('/:id')
     @HttpCode(HttpStatus.OK)
-    findById(@Param('id', ParseIntPipe) id: number): Promise <Categoria> {
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Categoria> {
         return this.service.findById(id)
     }
 
-    @Get('/nome/:nome')
+    @Get('/descricao/:descricao')
     @HttpCode(HttpStatus.OK)
-    findByNome(@Param('nome') nome: string): Promise <Categoria[]>{
-        return this.service.findByNome(nome)
+    findByDescricao(@Param('descricao') descricao: string): Promise<Categoria[]> {
+        return this.service.findByDescricao(descricao)
     }
 
     @Post()
     @HttpCode(HttpStatus.CREATED)
-    create(@Body() categoria: Categoria): Promise<Categoria>{
+    create(@Body() categoria: Categoria): Promise<Categoria> {
         return this.service.create(categoria)
     }
 
     @Put()
     @HttpCode(HttpStatus.OK)
-    update(@Body() categoria: Categoria): Promise<Categoria>{
+    update(@Body() categoria: Categoria): Promise<Categoria> {
         return this.service.update(categoria)
     }
 
     @Delete('/:id')
     @HttpCode(HttpStatus.NO_CONTENT)
-    delete(@Param('id', ParseIntPipe) id: number){
+    delete(@Param('id', ParseIntPipe) id: number) {
         return this.service.delete(id)
     }
 
-    
 }
